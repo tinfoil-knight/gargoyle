f3f5ac4d367f8514a0abaaaf1665a87681bf5673
Kunal Kundu <kunal99kundu@gmail.com>
2022-07-23 16:01:49 +0530

    add support for reverse-proxying a single target

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,25 +1,40 @@
 package server
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"net/http/httputil"
+	"net/url"
 )
 
 func NewHTTPServer() {
 	addr := ":8080"
 
+	proxy, err := newProxy("http://localhost:3030")
+	if err != nil {
+		panic(err)
+	}
+
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/", handleAllRequests)
+	mux.HandleFunc("/", handleAllRequests(proxy))
 
 	log.Printf("INFO: Starting server on %s", addr)
 	log.Fatal(http.ListenAndServe(addr, logHTTPRequest(mux)))
 }
 
-func handleAllRequests(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello %s!", r.UserAgent())
+func handleAllRequests(proxy *httputil.ReverseProxy) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		proxy.ServeHTTP(w, r)
+	}
+}
+
+func newProxy(targetHost string) (*httputil.ReverseProxy, error) {
+	url, err := url.Parse(targetHost)
+	if err != nil {
+		return nil, err
+	}
+	return httputil.NewSingleHostReverseProxy(url), err
 }
 
 func logHTTPRequest(handler http.Handler) http.Handler {
