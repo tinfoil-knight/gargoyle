b2416240e6ebc47fd8eba07f8c92f5cebdb28fef
Kunal Kundu <kunal99kundu@gmail.com>
2022-09-12 19:49:30 +0530

    refactor auth middleware

--- internal/server/middlewares.go
+++ internal/server/middlewares.go
@@ -37,28 +37,30 @@ func urlRewriter(handler http.Handler, rewriteCfg config.RewriteCfg) http.Handle
 }
 
 func auth(handler http.Handler, auth config.AuthConfig) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// TODO: refactor
-		if auth.BasicAuth != nil {
+	switch true {
+	case auth.BasicAuth != nil:
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			user, pwd, ok := r.BasicAuth()
 			hash, _ := auth.BasicAuth[user]
 			if !ok || !check(hash, pwd) {
 				http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
 				return
 			}
-		}
-		if auth.KeyAuth != nil {
-			header := auth.KeyAuth.Header
-			cfgKey := auth.KeyAuth.Key
+			handler.ServeHTTP(w, r)
+		})
+	case auth.KeyAuth != nil:
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			header, cfgKey := auth.KeyAuth.Header, auth.KeyAuth.Key
 			reqKey := r.Header.Get(header)
 			if cfgKey != reqKey {
 				http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
 				return
 			}
-		}
-
-		handler.ServeHTTP(w, r)
-	})
+			handler.ServeHTTP(w, r)
+		})
+	default:
+		panic(config.ErrInvalidConfig)
+	}
 }
 
 func check(hash []byte, pwd string) bool {
